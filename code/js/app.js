0ff9885d2620d1c93da464aeffe0444634f2e48d
YLUX-project <YLUXos.systeme@gmail.com>
2022-12-23 01:12:57 +0100

    Javascript commit

--- code/js/app.js
+++ code/js/app.js
@@ -2,46 +2,98 @@
 
 const swup = new Swup();
 
+// Var
+
+var create_room = $('#create');
+var join_page = $('#join');
+var join_room = $('#join-room');
+var pseudo = $('#pseudo');
+var room = $('#room');
+
 // Form
 
+console.log("Let's GOOO !");
+
+$(function () {
+
+    create_room.click(function () {
+        var roomId = Math.random().toString(36).substring(2);
+
+        console.log("roomId définit (create)");
+
+        // Envoyer une requête HTTP au serveur pour créer une nouvelle room avec cet identifiant
+        $.post('http://127.0.0.1:5500/code/room.html/', { id: roomId }, function () {
+            // Rediriger vers la page de la room en utilisant l'identifiant de room
+
+            pseudo.submit(function (event) {
+                event.preventDefault();
+                var username = pseudo.val(); // Récupérer le nom d'utilisateur entré par l'utilisateur
+
+                console.log("username récupéré (create)");
+
+                if (username && username.length <= 3) {
+                    localStorage.setItem('session', 'active');
+
+                    console.log("username validé (create)");
+
+                    window.location.href = `http://127.0.0.1:5500/code/room.html/${roomId}`
+                }
+                else {
+                    alert('Le nom d\'utilisateur doit faire minimum 3 caractères');
+
+                    console.log("username refusé (create)");
+                }
 
 
-$('#create').click(function () {
-    const roomId = Math.random().toString(36).substring(2);
-
-    // Envoyer une requête HTTP au serveur pour créer une nouvelle room avec cet identifiant
-    $.post('http://127.0.0.1:5500/room.html/', { id: roomId }, function (response) {
-        // Rediriger vers la page de la room en utilisant l'identifiant de room
-
-
-        $('#pseudo').submit(function (event) {
-            event.preventDefault();
-            var username = $('#username').val(); // Récupérer le nom d'utilisateur entré par l'utilisateur
-
-            if (username === 'test') {
-                localStorage.setItem('session', 'active');
-                window.location.href = `http://127.0.0.1:5500/room.html/${roomId}`
-            }
-            else {
-                alert('Nom d\'utilisateur incorrect');
-            }
-
-
+            });
         });
     });
 
+    join_page.click(function () {
+        pseudo.submit(function (event) {
+            event.preventDefault();
+            var username = pseudo.val(); // Récupérer le nom d'utilisateur entré par l'utilisateur
 
-})
+            console.log("username récupéré (join)");
 
-$(function () {
-    $('#join').click(function () {
-        $('#pseudo').submit(function (event) {
-            event.preventDefault(); // Empêcher la soumission du formulaire
-            var username = $('#username').val(); // Récupérer le nom d'utilisateur entré par l'utilisateur
+            if (username && username.length <= 3) {
+                localStorage.setItem('session', 'active');
 
+                console.log("username validé (join)");
 
+                window.location.href = `http://127.0.0.1:5500/code/room.html`
+
+                $(function () {
+                    join_room.click(function () {
+                        room.submit(function (event) {
+                            event.preventDefault();
+                            var roomId = room.val(); // Récupérer le code de session entré par l'utilisateur
+
+                            console.log("roomId récupéré (room)");
+
+                            if (roomId && roomId.length <= 3) {
+                                localStorage.setItem('session', 'active');
+
+                                console.log("roomId validé (room)");
+
+                                window.location.href = `http://127.0.0.1:5500/code/room.html/${roomId}`
+                            }
+                            else {
+                                alert('Nom de session incorrect');
+
+                                window.location.href = `http://127.0.0.1:5500/code/login.html`
+
+                                console.log("session refusé (room)");
+                            }
+                        });
+                    });
+                });
+            }
+            else {
+                alert('Le nom d\'utilisateur doit faire minimum 3 caractères');
+
+                console.log("username refusé (join)");
+            };
         });
-
-
-    })
+    });
 });
